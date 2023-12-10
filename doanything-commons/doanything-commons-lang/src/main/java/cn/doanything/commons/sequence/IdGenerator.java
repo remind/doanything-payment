dca4a8b4c1bf5171b87256e3889f247c905a44be
wxj <408934054@qq.com>
2023-12-10 16:20:53 +0800

    新增id生成器

---
+++ doanything-commons/doanything-commons-lang/src/main/java/cn/doanything/commons/sequence/IdGenerator.java
@@ -0,0 +1,47 @@
+package cn.doanything.commons.sequence;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.time.DateFormatUtils;
+
+import java.util.Date;
+
+/**
+ * id生成器
+ * @author wxj
+ * 2023/12/10
+ */
+public class IdGenerator {
+
+    /**
+     * 生成24位的ID，由 yyyyMMdd + 3位SystemCode + 3位bizType + 2位dbRouteId + 8位自增
+     *
+     * @param systemCode    系统编号
+     * @param bizType       业务类型
+     * @param dbRouteId     分表位
+     * @param sequence      自增序列
+     * @return  24位数字
+     */
+    public static String getId(String systemCode, String bizType, String dbRouteId, Long sequence) {
+        if (StringUtils.isBlank(systemCode) || systemCode.length() != 3) {
+            throw new IllegalArgumentException("系统编码长度不为3");
+        }
+
+        if (StringUtils.isBlank(bizType) || bizType.length() != 3) {
+            throw new IllegalArgumentException("业务类型编码长度不为3");
+        }
+
+        if (StringUtils.isBlank(dbRouteId) || dbRouteId.length() != 2) {
+            throw new IllegalArgumentException("分表位长度不为2");
+        }
+
+        if (sequence < 0 || sequence.toString().length() > 8) {
+            throw new IllegalArgumentException("序列必须为8位以内的正数");
+        }
+
+        return DateFormatUtils.format(new Date(), "yyyyMMdd") +
+                systemCode +
+                bizType +
+                dbRouteId +
+                String.format("%08d", sequence);
+    }
+}
