54a52e9b2586364409344da844914e4715cc23fa
wxj <408934054@qq.com>
2024-01-17 17:57:20 +0800

    dalgen调整

--- doanything-archetypes/doanything-archetype-service/src/main/resources/archetype-resources/gen/__rootArtifactId__-dalgen/src/main/java/dalgen/MyDalGenerator.java
+++ doanything-archetypes/doanything-archetype-service/src/main/resources/archetype-resources/gen/__rootArtifactId__-dalgen/src/main/java/dalgen/MyDalGenerator.java
@@ -1,7 +1,7 @@
-#set( $symbol_pound = '#' )
-#set( $symbol_dollar = '$' )
-#set( $symbol_escape = '\' )
-package ${package}.dalgen;
+#set($symbol_pound='#')
+        #set($symbol_dollar='$')
+        #set($symbol_escape='\' )
+        package ${package}.dalgen;
 
 import cn.doanything.framework.generator.DalGenerator;
 import cn.doanything.framework.generator.config.GenConfig;
@@ -12,12 +12,12 @@ import java.io.File;
 /**
  * mybatis自动生成代码
  */
-public class DalGenerator {
+public class MyDalGenerator {
 
     public static void main(String[] args) {
-        GenConfig genConfig =  YamlUtil.loadByPath("config.yaml", GenConfig.class);
+        GenConfig genConfig = YamlUtil.loadByPath("config.yaml", GenConfig.class);
         DalGenerator dalGenerator = new DalGenerator(genConfig);
-        File f = new File(BasicGenerator.class.getResource("/").getPath());
+        File f = new File(MyDalGenerator.class.getResource("/").getPath());
         String projectPath = f.getParentFile().getParentFile().getParentFile().getParentFile().getPath();
         dalGenerator.execute(projectPath, "md");
     }
