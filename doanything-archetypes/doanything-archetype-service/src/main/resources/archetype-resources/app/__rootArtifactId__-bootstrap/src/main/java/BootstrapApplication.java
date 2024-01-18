da38b8170e59f8054a5efc9746feed496fb83dc2
wxj <408934054@qq.com>
2024-01-18 17:38:39 +0800

    新增mybatis扫描

--- doanything-archetypes/doanything-archetype-service/src/main/resources/archetype-resources/app/__rootArtifactId__-bootstrap/src/main/java/BootstrapApplication.java
+++ doanything-archetypes/doanything-archetype-service/src/main/resources/archetype-resources/app/__rootArtifactId__-bootstrap/src/main/java/BootstrapApplication.java
@@ -5,12 +5,14 @@ package ${package};
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @Slf4j
 @EnableDubbo
+@MapperScan("cn.doanything.${rootArtifactId}.infrastructure.persistence.*.mapper")
 public class BootstrapApplication {
 
     public static void main(String[] args) {
